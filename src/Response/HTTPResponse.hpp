50087cf2ca60a5d96107b598a9dff8d8c78fa397
omhs-dev <hsouley@gmail.com>
2024-11-01 01:16:28 +0100

    added some task to do

--- src/Response/HTTPResponse.hpp
+++ src/Response/HTTPResponse.hpp
@@ -1,68 +1,3 @@
-// #ifndef HTTPRESPONSE_HPP
-// #define HTTPRESPONSE_HPP
-
-// #include <string>
-// #include <fstream>
-// #include <sstream>
-// #include <sys/stat.h>
-// #include <vector>
-// #include "../Server/Client.hpp"
-// #include "../Request/HTTPRequest.hpp"
-
-// // function that generates responses based on the req
-//    // function to handle delete req
-//    // function to handle put req
-//    // function to handle get req
-//    // for the function handleGet req
-//        // a function that checks if req is directed to the server block
-//            // first retrieve the path to all servers in a list
-//            //Find the path that matches the req path
-//            //Once okay
-//            //Check if the req path points to a large file
-//                //If yes prepare a chunked response
-//            // otherwise prepare a standard response
-
-// class Client;
-// class HTTPRequest;
-
-// class HTTPResponse {
-//     private:
-//         Client *_client;
-//         HTTPRequest* _request;
-		
-//         std::string _statusCode;
-//         std::string _statusMessage;
-//         std::string _header;
-//         std::string _body;
-//         std::string _response;
-
-//     public:
-//         HTTPResponse();
-
-//         //Setters
-//         void setStatus(const std::string& code, const std::string& message);
-//         void setDefaultResponse();
-//         void setStandardResponse(const std::string &path);
-//         void setHeader(const std::string& header);
-//         void setBody(const std::string& body);
-
-//         // HTTPRequest* getRequest() (return _request);
-//         // Gets the full HTTP response data (headers + body)
-//         std::string getData() const;
-//         void generateResponse(const std::string &path);
-//         void handleGetRequest(const std::string &path);
-
-    
-//         void setError(int code, bool errorPage);
-
-
-// };
-
-// #endif
-
-
-
-// HTTPResponse.hpp
 #ifndef HTTPRESPONSE_HPP
 #define HTTPRESPONSE_HPP
 
@@ -84,8 +19,12 @@
 class HTTPRequest;
 class Client;
 
+// have an instance of Cgi
+
 // Set a content type based on the file extension
 // set an error page message
+
+// a function to set the a chunked header
 
 // Generate reponse
 	// handle methodes
@@ -95,16 +34,16 @@ class Client;
 			// if path is file or different of "/"
 				// check if the path is a good path
 					// if yes
-						// check if the file is large
-							// if yes prepare chunked response
 						// check if the path is a file
 							// if yes serve the file
+						// check if the file is large
+							// if yes prepare chunked response
 						// check if the path is a directory
 							// check if the directory has an index file
 								// if yes serve the index file
 							// check if the directory has an autoindex on or off
 								// if yes serve the directory
-						// if is a recirect
+						// if is a redirect
 							// if yes serve the redirect
 					// if no serve 404
 
